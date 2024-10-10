049125d2e4242b8ec07f9aac61e6652e9a6f79a8
redapp1 <ridwan.taza@gmail.com>
2024-10-10 23:25:34 +0200

    feat(shop): add products list at home page
    - display pertinent product's informations on the card.
    - make cards list responsive.
    - add scrolling to products list container
    - add an image to product.json for test

--- front/src/app/shared/features/home/home.component.ts
+++ front/src/app/shared/features/home/home.component.ts
@@ -1,15 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
+import { TagModule } from "primeng/tag";
+import { ProductsService } from "app/products/data-access/products.service";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
   standalone: true,
-  imports: [CardModule, RouterLink, ButtonModule],
+  imports: [CardModule, RouterLink, ButtonModule, TagModule],
 })
 export class HomeComponent {
   public readonly appTitle = "ALTEN SHOP";
+
+  private readonly productsService = inject(ProductsService);
+
+  public readonly products = this.productsService.products;
+
+  public readonly INVENTORY_STATUS_SEVERTY_MAP = {
+    INSTOCK: 'success' as const,
+    LOWSTOCK: 'warning' as const,
+    OUTOFSTOCK: 'danger' as const,
+  }
+
+  ngOnInit() {
+    this.productsService.get().subscribe();
+  } 
 }
+
+
