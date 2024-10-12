4d2032a0379c3f5138914bc31f7573ea41557fa6
redapp1 <ridwan.taza@gmail.com>
2024-10-12 23:36:18 +0200

    feat(contact): add contact form

--- front/src/app/shared/features/contact/contact.component.ts
+++ front/src/app/shared/features/contact/contact.component.ts
@@ -1,15 +1,28 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
+import { InputTextModule } from "primeng/inputtext";
+import { InputTextareaModule } from "primeng/inputtextarea";
 
 @Component({
-  selector: "app-home",
+  selector: "app-contact",
   templateUrl: "./contact.component.html",
   styleUrls: ["./contact.component.scss"],
   standalone: true,
-  imports: [ CardModule ],
+  imports: [CardModule, ButtonModule, InputTextModule, InputTextareaModule, FormsModule, ReactiveFormsModule ],
 })
 export class ContactComponent {
   public readonly pageTitle = "Contactez Nous";  
+
+  public contactForm  = new FormGroup({
+    email: new FormControl(''),
+    message: new FormControl(''),
+  });
+
+  sendMessage() {
+    console.log( this.contactForm.value )
+  }
 }
 
 
