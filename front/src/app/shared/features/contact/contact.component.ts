c35ba201b0dac759e98f13bdbfe0f75734d4d464
redapp1 <ridwan.taza@gmail.com>
2024-10-13 00:23:06 +0200

    feat(contact): add form validation

--- front/src/app/shared/features/contact/contact.component.ts
+++ front/src/app/shared/features/contact/contact.component.ts
@@ -1,5 +1,6 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { InputTextModule } from "primeng/inputtext";
@@ -10,18 +11,30 @@ import { InputTextareaModule } from "primeng/inputtextarea";
   templateUrl: "./contact.component.html",
   styleUrls: ["./contact.component.scss"],
   standalone: true,
-  imports: [CardModule, ButtonModule, InputTextModule, InputTextareaModule, FormsModule, ReactiveFormsModule ],
+  imports: [ CommonModule, CardModule, ButtonModule, InputTextModule, InputTextareaModule, FormsModule, ReactiveFormsModule ],
 })
 export class ContactComponent {
   public readonly pageTitle = "Contactez Nous";  
 
+  public email = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ])
+
+  public message = new FormControl('', [
+    Validators.required,
+    Validators.maxLength( 300 )
+  ])
+
   public contactForm  = new FormGroup({
-    email: new FormControl(''),
-    message: new FormControl(''),
+    email: this.email,
+    message: this.message
   });
 
   sendMessage() {
-    console.log( this.contactForm.value )
+    if ( this.email.valid && this.message.valid ) {
+      console.log( this.contactForm.value )
+    }
   }
 }
 
