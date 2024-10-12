f67a125c23376b3998077edcff95f762a439ac81
redapp1 <ridwan.taza@gmail.com>
2024-10-12 16:16:56 +0200

    feat(shop): Display a badge indicating the quantity of products in the cart

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,18 +1,26 @@
 import {
   Component,
+  computed,
+  inject,
 } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { SplitterModule } from 'primeng/splitter';
 import { ToolbarModule } from 'primeng/toolbar';
 import { PanelMenuComponent } from "./shared/ui/panel-menu/panel-menu.component";
+import { BadgeModule } from 'primeng/badge';
+import { ShoppingCartService } from "./shopping-cart/shopping-cart.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
   standalone: true,
-  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent],
+  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent, BadgeModule],
 })
 export class AppComponent {
   title = "ALTEN SHOP";
+
+  private readonly shoppingCartService = inject( ShoppingCartService )
+
+  public totalProductsInCart = computed( () => this.shoppingCartService.cartItems()?.length || 0 )
 }
