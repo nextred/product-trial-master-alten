7edda3419f0038a336742c087b55fb895b1fb380
nextred <redouan.elmossaoui@gmail.com>
2024-10-14 14:51:46 +0200

    feat(shop): add a pagination system
    - display skeleton component when page load in progress

--- front/src/app/products/data-access/products.service.ts
+++ front/src/app/products/data-access/products.service.ts
@@ -14,12 +14,47 @@ import { catchError, Observable, of, tap } from "rxjs";
 
     public readonly products = this._products.asReadonly();
 
+    private readonly _productsPerPage = signal<{ [key: number]: Product[]}>({});
+
+    public readonly productsPerPage = this._productsPerPage.asReadonly();
+
+    private readonly _productsTotal = signal<number>(0);
+
+    public readonly productsTotal = this._productsTotal.asReadonly();
+
     public get(): Observable<Product[]> {
         return this.http.get<Product[]>(this.path).pipe(
             catchError((error) => {
                 return this.http.get<Product[]>("assets/products.json");
             }),
             tap((products) => this._products.set(products)),
+        );
+    }
+
+    public getByPage( params: { limit: number, offset: number, page: number } ) {
+
+        let path = `${this.path}?limit=${params.limit}&offset=${params.offset}`
+
+        return this.http.get<{ products: Product[], total: number }>(path).pipe(
+            catchError(( error ) => {
+                return new Observable<{ products: Product[], total: number }> ((subscriber) => {
+                    this.http.get<Product[]>("assets/products.json").subscribe( (items) => {
+                        let total    = items.length
+                        let products = items.splice( params.offset, params.limit )
+
+                        setTimeout( () => {
+                            subscriber.next({ products, total })
+                            subscriber.complete()
+                        }, 1000)   
+
+                    })
+                })
+            }),
+
+            tap( ( item: { products: Product[], total: number } ) => {
+                this._productsTotal.set(item.total)
+                this._productsPerPage.update(products => ( { ...products, [params.page]: item.products } ))
+            })
         );
     }
 
--- front/src/app/shared/features/home/home.component.ts
+++ front/src/app/shared/features/home/home.component.ts
@@ -1,18 +1,28 @@
-import { Component, inject } from "@angular/core";
+import { Component, computed, inject, signal } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { TagModule } from "primeng/tag";
+import { PaginatorModule } from 'primeng/paginator';
+import { SkeletonModule } from 'primeng/skeleton';
 import { ProductsService } from "app/products/data-access/products.service";
 import { Product } from "app/products/data-access/product.model";
 import { ShoppingCartService } from "app/shopping-cart/data-access/shopping-cart.service";
+import { CommonModule } from "@angular/common";
+
+interface PageEvent {
+  first?: number | undefined;
+  rows?: number | undefined;
+  page?: number | undefined;
+  pageCount?: number | undefined;
+}
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
   standalone: true,
-  imports: [CardModule, RouterLink, ButtonModule, TagModule],
+  imports: [CommonModule, CardModule, RouterLink, ButtonModule, TagModule, PaginatorModule, SkeletonModule],
 })
 export class HomeComponent {
   public readonly appTitle = "ALTEN SHOP";
@@ -40,9 +50,35 @@ export class HomeComponent {
   removeProductFromCart( product: Product ) {
     this.shoppingCartService.remove( product.id )
   }
+
+  /***** pagination section ******/
+
+  // products per page
+  rows: number = 8;
+  
+  // index of current page first item
+  public first = signal<number>(0);
+
+  // page number ( start from 0)
+  public page = signal<number>(0);
+
+  productsPerPage = computed( () => this.productsService.productsPerPage()[this.page()] )
+
+  totalRecords = this.productsService.productsTotal
+
+  onPageChange(event: PageEvent) {
+    if ( event.first !== undefined )
+      this.first.set(event.first);
+
+    if ( event.page !== undefined )
+      this.page.set(event.page)
+
+    if ( !this.productsPerPage() )
+      this.productsService.getByPage( { limit: this.rows, offset: this.first(), page: this.page() }).subscribe()
+  }
   
   ngOnInit() {
-    this.productsService.get().subscribe();
+    this.productsService.getByPage( { limit: this.rows, offset: this.first(), page: this.page() }).subscribe();
   } 
 }
 
